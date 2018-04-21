9be11d7d46fdfe7326bbdb3ea2c0c850afe1daea
Antoine Thiry <thiryantoine@gmail.com>
2018-04-21 15:47:24 +0200

    Priest + UI

---
+++ Assets/Scripts/Hero/Priest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Priest : Hero {
+
+	// Use this for initialization
+    protected override void Start () {
+		
+	}
+	
+	// Update is called once per frame
+    protected override void Update () {
+		
+	}
+
+    protected override void DoActions()
+    {
+        base.DoActions();
+        Debug.Log("Action from priest");
+    }
+}

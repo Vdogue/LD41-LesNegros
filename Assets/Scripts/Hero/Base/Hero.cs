8bb5541db0349ad6679c67b0d44e7e424ca67c6c
Antoine Thiry <thiryantoine@gmail.com>
2018-04-21 13:50:01 +0200

    First commit

---
+++ Assets/Scripts/Hero/Base/Hero.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour
+{
+    [Header("Stats")]
+    public Stats stats;
+
+    public virtual void DoActions()
+    {
+        Debug.Log("Action");
+    }
+
+    void OnMouseDown()
+    {
+        DoActions();
+    }
+}
---
+++ Assets/Scripts/Hero/Base/Stats.cs
@@ -0,0 +1,7 @@
+﻿public class Stats
+{
+    public float health;
+    public float dexterity;
+    public float strenght;
+    public float intellect;
+}
